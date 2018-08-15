068c5332b9b739ff6f6a9540b5250eb52c2c56c7
QianGuoqing <qian_guoqing@yeah.net>
2018-08-15 13:40:45 +0800

    初始化项目

---
+++ lazy-fish/src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
+import './common/css/reset.css'
+
+Vue.config.productionTip = false
+
+fastclick.attach(document.body)
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  attempt: 1
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
