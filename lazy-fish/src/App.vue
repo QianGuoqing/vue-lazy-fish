8d1e110ff217caab5956e8e2dfd00b4ef6d2b94e
QianGuoqing <qian_guoqing@yeah.net>
2018-08-15 14:02:21 +0800

    添加header部分

--- lazy-fish/src/App.vue
+++ lazy-fish/src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <fish-header></fish-header>
+    <div class="app-main">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import FishHeader from './components/fish-header/FishHeader.vue'
+  export default {
+    name: 'App',
+    components: {
+      FishHeader
+    }
+  }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+  .app-main
+    margin-top 56px
 </style>
---
+++ lazy-fish/src/components/fish-header/FishHeader.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="fish-header">
+    <img src="./images/二维码.png" class="scan-icon" alt="">
+    <div class="search-wrapper">
+      <img src="./images/search.png" class="search-icon" alt="">
+      <input class="search-input" placeholder="搜索宝贝/鱼塘/用户" type="text"/>
+    </div>
+    <img src="./images/分类.png" class="category-icon" alt="">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'FishHeader'
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '../../common/stylus/variables.styl';
+  .fish-header
+    width 100%
+    height 56px
+    background-color $theme-color
+    display flex
+    align-items center
+    position fixed
+    left 0
+    top 0
+    right 0
+    .scan-icon, .category-icon
+      width 22.5px
+      height 22.5px
+      margin 0 15px
+    .search-wrapper
+      flex 1
+      position relative
+      .search-icon
+        width 22px
+        height 22px
+        position absolute
+        top 6px
+        left 5px
+      .search-input
+        width 86%
+        height 30px
+        border-radius 5px
+        outline none
+        border none
+        padding-left 35px
+</style>
